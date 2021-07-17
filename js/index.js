6abcb9cdae3910734dfd30796aa86ce3d34d2aff
Victor Bispo <victor.sp@outlook.com.br>
2021-07-17 03:22:07 -0300

    last commit

--- js/index.js
+++ js/index.js
@@ -1,10 +1,10 @@
 const form = document.getElementById('myForm')
 
-form.addEventListener('submit', (e) =>{
+form.addEventListener('submit', (e) => {
     e.preventDefault();
 
     let nome = document.getElementById('nome-form').value;
-    let email = document.getElementById('email-form').value;    
+    let email = document.getElementById('email-form').value;
     let data = {
         nome,
         email,
@@ -16,7 +16,13 @@ form.addEventListener('submit', (e) =>{
 
     let content = document.getElementById('content-form');
 
-    let carregado =  '<p>carregando..</p>'
+    let carregado = '<p>Enviando...</p>'
+
+    let enviado = `<p>Enviado, verifique em seu e-mail!</p>`
 
     content.innerHTML = carregado
+
+    setTimeout(() => {
+        content.innerHTML = enviado
+    }, 1000)
 })
